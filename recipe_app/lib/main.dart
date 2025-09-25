d320d589b6008089bab85c46139b57306214098c
도경원 <won03289@gmail.com>
2025-09-25 09:36:25 +0900

    Row Column위젯 학습

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -5,11 +5,31 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: Placeholder()),
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Woman"),
+                    Text("Kids"),
+                    Text("Shoes"),
+                    Text("Bag"),
+                  ],
+                ),
+              ),
+              Expanded(child: Placeholder(), flex: 1),
+              Expanded(child: Placeholder(), flex: 1),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
