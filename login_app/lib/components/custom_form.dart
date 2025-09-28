8795c152fa960232436d05a8381efc69c7961a0f
도경원 <won03289@gmail.com>
2025-09-29 01:12:01 +0900

    로그인 앱 만들기 완료

---
+++ login_app/lib/components/custom_form.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:login_app/components/custom_text_form_field.dart';
+import 'package:login_app/size.dart';
+
+class CustomForm extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>(); // 1. 글로벌 key
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      // 2. 글로벌 key를 Form 태그에 연결하여 해당 key로 Form의 상태를 관리할 수 있다.
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomTextFormField("Email"),
+          SizedBox(height: medium_gap),
+          CustomTextFormField("Password"),
+          SizedBox(height: large_gap),
+          TextButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                // 유효성 검사
+                Navigator.pushNamed(context, "/home");
+              }
+            },
+            child: Text("Login"),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ login_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:login_app/pages/home_page.dart';
+import 'package:login_app/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // 테마 설정
+      theme: ThemeData(
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            minimumSize: Size(400, 60),
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
+    );
+  }
+}
